451ab1a06be6af7626192b049766702624282a29
Aadil Sidik <aadilsidik2@gmail.com>
2025-11-28 20:38:02 +0000

    feat: Parse html and get every http <a> tag

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gocrawl
 
 go 1.25.4
+
+require golang.org/x/net v0.47.0
--- main.go
+++ main.go
@@ -3,22 +3,70 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
+var AllowedProtocols = [...]string{
+	"http",
+	"https",
+}
+
+func main() {
+	/*reader := bufio.NewReader(os.Stdin)
+	fmt.Print("URL to crawl: ")
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error reading input")
+	}*/
+
+	CrawlSite("https://www.webscraper.io")
+}
+
 func CrawlSite(url string) {
-	resp, err := http.Get("https://www.webscraper.io")
+	resp, err := http.Get(url)
 
 	if err != nil {
 		fmt.Println("Error")
 	}
 	defer resp.Body.Close()
 
-	rb, err := io.ReadAll(resp.Body)
-
-	fmt.Println(string(rb))
+	aTags(resp.Body)
 }
 
-func main() {
+func aTags(body io.ReadCloser) {
+	doc, err := html.Parse(body)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for n := range doc.Descendants() {
+		if n.Type == html.ElementNode && n.DataAtom == atom.A {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					val := a.Val
+					protocol := strings.Split(val, `:`)[0]
+
+					if protocolAllowed(protocol) {
+						fmt.Println(string(a.Val))
+					}
+				}
+			}
+		}
+	}
+}
+
+func protocolAllowed(proto string) bool {
+	for _, p := range AllowedProtocols {
+		if p == proto {
+			return true
+		}
+	}
+	return false
 }
